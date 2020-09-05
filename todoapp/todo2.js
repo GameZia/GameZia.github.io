12258e117897a1da3f6d9ddaa6623df93179193c
GameZia <70689587+GameZia@users.noreply.github.com>
2020-09-05 22:56:21 +0530

    Update todo2.js

--- todoapp/todo2.js
+++ todoapp/todo2.js
@@ -2,23 +2,68 @@ $("ul").on("click","li",function()
 {
 	$(this).toggleClass("completed");
 });
+var form = document.querySelector('.to-do-form');
+var itm = document.querySelector('.todoitem');
+var input = document.querySelector('.input');
+var todos = [];
+form.addEventListener('submit',function(event){
+	event.preventDefault();
+	addToDo(input.value);
+});
+function addToDo(string){
+	if(string!=""){
+		const todo = {
+			id:Date.now(),
+			name:string,
+			completed:false
+		};
+		todos.push(todo);
+		addToLocalStorage(todos);
+		input.value = "";
+	}
+}
+function renderTodos(item){
+	itm.innerHTML = "";
+	item.forEach(element => {
+		const checked = element.completed?'checked':null;
+		const li = document.createElement('li');
+		li.setAttribute('data-key',element.id);
+		if(element.completed===true){
+			li.classList.add('completed');
+		}
+		li.innerHTML = `<span><i class="fa fa-trash"></i></span> ${element.name} `;
+		itm.append(li);
+
+	});
+}
+function addToLocalStorage(todos){
+	localStorage.setItem('todos',JSON.stringify(todos));
+	renderTodos(todos);
+}
+function getFromStorage(){
+	const reference = localStorage.getItem('todos');
+	if(reference){
+		todos = JSON.parse(reference);
+		renderTodos(todos);
+	}
+}
 $("ul").on("click","span",function(event)
 {
-$(this).parent().fadeOut(1000,function()
+$(this).parent().fadeOut(750,function()
 	{
+		deleteTodo($(this).attr('data-key'));
 		$(this).remove();
 	});
 event.stopPropagation();
 });
-$("textarea").on("keypress",function(event)
-{
-	if(event.which===13)
-	{
-		var todoText=$(this).val();
-		$("ul").append("<li><span><i class='fa fa-trash'> </i></span>"+todoText+"</li>")
-	}
-
-})
+function deleteTodo(id){
+	todos = todos.filter(function(item){
+		return item.id!=id;
+	});
+	addToLocalStorage(todos);
+	console.log("deleted successfully");
+}
+getFromStorage();
 function toggleslidebar() {
 	document.getElementById("slidebar").classList.toggle('active');
 }
